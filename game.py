954102da9466976c684b8001009f3c100379ce6b
DogeSec <utdoge@gmail.com>
2018-03-06 17:40:46 -0700

    Parsed instead of globalization

--- game.py
+++ game.py
@@ -1,22 +1,20 @@
 import random
-money = 0
 def start():
   print('Hello! The objective of the game is to collect money')
   a = input("Do you want to play?")
-  if a.lower() in ('y', 'yes', 'Y'):
+  if a.lower() in ('y', 'yes'):
     Game()
-def Game():
-    global money 
+def Game(money):
     number = random.randint(1,50)
     pick = input("Do you want to pick money?")
     if pick.lower() in ('y', 'Y', 'yes'):
       money = money+number
       print(f"You caught {number} bucks\n You currently have {money} bucks")
-      Game()
+      Game(money)
     else:
       invest = input("Want to double your earnings?")
       if invest.lower() in ('y', 'yes'):
         print(f"You just got {money} !")
         money *= 2 
-        Game()
-start()
+        Game(money)
+start(0)
