4d2a3b7f97a7f7ea5bda614d1f50dc2ce70203d9
DogeSec <utdoge@gmail.com>
2018-03-12 06:58:39 -0600

    Made the code less complex. Made a better loop.

--- game.py
+++ game.py
@@ -1,20 +1,14 @@
 import random
-def start():
-  print('Hello! The objective of the game is to collect money')
-  a = input("Do you want to play?")
-  if a.lower() in ('y', 'yes'): #If user inputs one of the following, game starts with 0 money.
-    Game(0)
-def Game(money):
-    number = random.randint(1,51) #generate a random number 
+
+balance = 0
+while True:
+    number = random.randint(1,51)
     pick = input("Do you want to pick money?")
     if pick.lower() in ('y', 'yes'): 
-      money += number
-      print(f"You caught {number} bucks\nYou currently have {money} bucks")
-      Game(money)
+      balance += number
+      print(f"You caught {number} bucks\nYou currently have {balance} bucks")
     else:
       invest = input("Want to double your earnings?")
       if invest.lower() in ('y', 'yes'): 
-        print(f"You just got {money}!")
-        money *= 2 
-        Game(money) 
-start()
+        print(f"You just got {balance}!")
+        balance *= 2 
