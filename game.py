2239ef7a9810af68cd2c5ab804a3345d21914aae
DogeSec <utdoge@gmail.com>
2018-04-15 20:01:48 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,16 @@
+# HyperNovae
 import random
 
 balance = 0
+def invest(balance):
+  balance *= 2
+  print(f"You now have {balance}!")
+  return balance
 while True:
     number = random.randint(1,51)
-    pick = input("Do you want to pick money?")
+    pick = input("Do you want to pick money? (input enter to instantly invest!)")
     if pick.lower() in ('y', 'yes'): 
       balance += number
       print(f"You caught {number} bucks\nYou currently have {balance} bucks")
     else:
-      invest = input("Want to double your earnings?")
-      if invest.lower() in ('y', 'yes'): 
-        print(f"You just got {balance}!")
-        balance *= 2 
+      balance += invest(balance)
