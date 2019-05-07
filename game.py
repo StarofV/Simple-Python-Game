27eda689e75e89f1bbb628f1d4defc57225c0f57
DogeSec <utdoge@gmail.com>
2019-05-07 09:22:33 -0600

    Added 50% chance gamble!
    Now, to achieve other tasks.

--- game.py
+++ game.py
@@ -5,25 +5,32 @@ import json
 # Load that sexy cash!
 with open('lastScore.json', 'r') as infile:
     inst = json.load(infile)
+
 # Define that sexy cash!    
 balance = inst['score'] 
 
 # Double that sexy cash!
-def invest(balance):
-  balance *= 2
-  print(f"You now have {balance}!")
-  return balance
+def invest(bal):
+  chance = random.randrange(1,11)
+  if chance < 5: # 10% chance.. I think
+    bal = 2
+    print(f"You now have {balance * 2}!")
+    return bal
+  else:
+    bal = 0
+    print(f"You lost everything... Now you have {bal}")
+    return bal
 
 # Pick up that sexy cash!
 while True:
     try:
-        number = random.randint(1,51) # Change this value and I will call the FBI on you
-        pick = input("Do you want to pick money? (input enter to instantly invest!)")
+        number = random.randrange(1,10) # Change this value and I will call the FBI on you
+        pick = input("Do you want to pick money? (input enter to instantly invest!)\n")
         if pick.lower() in ('y', 'yes'): 
           balance += number
-          print(f"You caught {number} bucks\nYou currently have {balance} bucks.") # Good job. Might add 50% for a fail.
+          print(f"You caught {number} bucks.\nBalance = {balance}.")
         else:
-          balance += invest(balance)
+          balance *= invest(balance)
     except KeyboardInterrupt:
         with open('lastScore.json', 'w') as outfile: # Save that sexy cash!
             data = {'score': balance}
