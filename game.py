d15b41ff0fea8775594b4827c0dcea41d52bfbfe
DogeSec <utdoge@gmail.com>
2019-05-07 09:23:34 -0600

    Last commit was an accident
    Hit goal of under 40 lines.

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 # Written by ThePythonDoge. You can do anything with this code. It probably can be shortened to 10 lines. 
-import random
-import json
+import random, json
 
 # Load that sexy cash!
 with open('lastScore.json', 'r') as infile:
@@ -11,20 +10,15 @@ balance = inst['score']
 
 # Double that sexy cash!
 def invest(bal):
-  chance = random.randrange(1,11)
-  if chance < 5: # 10% chance.. I think
-    bal = 2
-    print(f"You now have {balance * 2}!")
-    return bal
-  else:
-    bal = 0
-    print(f"You lost everything... Now you have {bal}")
-    return bal
+  chance = random.randrange(1,11) # 50% of winning
+  bal = 2 if chance < 5 else 0
+  print(f"You doubled to {balance * 2}!" if bal == 2 else "You lost everything...")
+  return bal
 
 # Pick up that sexy cash!
 while True:
     try:
-        number = random.randrange(1,10) # Change this value and I will call the FBI on you
+        number = random.randrange(1,10) #Change this value and I will call the FBI on you
         pick = input("Do you want to pick money? (input enter to instantly invest!)\n")
         if pick.lower() in ('y', 'yes'): 
           balance += number
