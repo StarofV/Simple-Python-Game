8eae462fb649654490144847f363ae05751a48c3
DogeSec <utdoge@gmail.com>
2019-05-08 07:52:03 -0600

    More cleaning up

--- game.py
+++ game.py
@@ -3,32 +3,28 @@ import random, json
 
 # Load that sexy cash!
 with open('lastScore.json', 'r') as infile:
-    inst = json.load(infile)
-
-# Define that sexy cash!    
-balance = inst['score'] 
+    inst = json.load(infile)    
+    balance = inst['score'] 
 
 # Double that sexy cash!
 def invest(bal):
-  chance = random.randrange(1,11) # 50% of winning
-  bal = 2 if chance < 5 else 0
+  chance = random.choice([True, False]) # 50% of winning
+  bal = 2 if chance == True else 0
   print(f"You doubled to {balance * 2}!" if bal == 2 else "You lost everything...")
   return bal
 
 # Pick up that sexy cash!
 while True:
     try:
-        number = random.randrange(1,10) #Change this value and I will call the FBI on you
         pick = input("Do you want to pick money? (input enter to instantly invest!)\n")
         if pick.lower() in ('y', 'yes'): 
+          number = random.randrange(1,11)
           balance += number
           print(f"You caught {number} bucks.\nBalance = {balance}.")
         else:
-          balance *= invest(balance)
-    except KeyboardInterrupt:
-        with open('lastScore.json', 'w') as outfile: # Save that sexy cash!
+          balance *= invest(balance) # multiply the number returned
+    except KeyboardInterrupt: #Save and exit
+        with open('lastScore.json', 'w') as outfile:
             data = {'score': balance}
             json.dump(data, outfile, sort_keys = True, indent = 4) # Make it neat!
-        exit(2) # Kill that sexy process!
-
-# Am I commenting correctly?
+        exit()
