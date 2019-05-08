e465f2e762b3003c6c834155050bed95bf0c9f12
DogeSec <utdoge@gmail.com>
2019-05-08 14:53:20 -0600

    Update game.py

--- game.py
+++ game.py
@@ -11,18 +11,26 @@ def invest(bal):
   chance = random.choice([True, False]) # 50% of winning
   bal = 2 if chance == True else 0
   print(f"You doubled to {balance * 2}!" if bal == 2 else "You lost everything...")
+
   return bal
 
 # Pick up that sexy cash!
 while True:
     try:
         pick = input("Do you want to pick money? (input enter to instantly invest!)\n")
-        if pick.lower() in ('y', 'yes'): 
+        pick_chance = random.choice([True,False])
+        
+        if pick.lower() in ('y', 'yes') and pick_chance == True: 
           number = random.randrange(1,11)
           balance += number
           print(f"You caught {number} bucks.\nBalance = {balance}.")
+        
+        elif pick.lower() in ('y', 'yes') and pick_chance == False:
+          print("Oh no, you didn't catch any. Try again!")
+        
         else:
           balance *= invest(balance) # multiply the number returned
+    
     except KeyboardInterrupt: #Save and exit
         with open('lastScore.json', 'w') as outfile:
             data = {'score': balance}
